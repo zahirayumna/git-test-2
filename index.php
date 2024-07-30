f68f572219c605fcf792390aa274b288266a5351
Zahira Putri <zahiraputri.project@gmail.com>
2024-07-30 20:45:45 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, suscipit possimus. Officia est, soluta, incidunt sit eius rerum animi explicabo, inventore aspernatur rem suscipit non possimus consequuntur dicta ullam id.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
